71b0d8db028553ba4dba2289e2356aeef5c34196
ΚΩΝΣΤΑΝΤΙΝΟΣ ΓΚΟΥΣΑΡΗΣ <cs171073@uniwa.gr>
2020-08-28 17:52:42 +0300

    Add project files.

---
+++ Graduate/main.cpp
@@ -0,0 +1,20 @@
+/*
+	Project_1 'Graduate' - Subject July / 2020 - Object Oriented Programing - C++
+
+	Konstantinos Gousaris - University Of West Attica
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "Project Graduate" << endl;
+
+
+
+
+
+
+	return 0;
+}
